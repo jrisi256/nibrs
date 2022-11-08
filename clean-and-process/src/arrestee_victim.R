2159f74867a0dc15fcce3da4935d03327a383888
jrisi256 <jrisi256@gmail.com>
2022-11-08 12:14:04 -0500

    Picking this project back up.

--- clean-and-process/src/arrestee_victim.R
+++ clean-and-process/src/arrestee_victim.R
@@ -2,7 +2,10 @@ library(here)
 library(arrow)
 library(dplyr)
 library(tidyr)
+library(purrr)
+library(stringr)
 library(ggplot2)
+library(forcats)
 
 # Directory for parquet files
 arrestees_dir <- here("clean-and-process",
@@ -17,7 +20,7 @@ victims_dir <- here("clean-and-process",
   
 ######################################################## Schema for victims
 victim_schema <- schema(ori = string(),
-                        year = double(),
+                        year = string(),
                         state = string(),
                         state_abb = string(),
                         incident_number = string(),
@@ -70,6 +73,9 @@ victim_schema <- schema(ori = string(),
                         unique_incident_id = string())
 
 ################################################### Schema for those arrestees
+# ori, year, state, incident_number, incident_date, arrest_date,
+# ucr_arrest_offense_code, age_of_arrestee, sex_of_arrestee, race_of_arrestee,
+# ethnicity_of_arrestee, resident_status_of_arrestee, unique_incident_id
 arrestee_schema <- schema(ori = string(),
                           year = string(),
                           state = string(),
@@ -94,39 +100,188 @@ arrestee_schema <- schema(ori = string(),
                           disposition_of_arrestee_under18 = string(),
                           unique_incident_id = string())
   
-arrestees <- open_dataset(here(arrestees_dir), schema = arrestee_schema)
-victims <- open_dataset(here(victims_dir), schema = victim_schema)
-
-arrestees_df <-
-    arrestees %>%
-    select(ori, year, state, incident_number, incident_date, arrest_date,
-           ucr_arrest_offense_code, age_of_arrestee, sex_of_arrestee,
-           race_of_arrestee, ethnicity_of_arrestee, resident_status_of_arrestee,
-           unique_incident_id) %>%
-    count(resident_status_of_arrestee) %>%
-    collect() %>%
-    rename(arrestee = resident_status_of_arrestee) %>%
-    mutate(arrestee = case_when(!(arrestee %in% c("resident", "nonresident")) ~ "unknown",
-                                is.na(arrestee) ~ "unknown",
-                                T ~ arrestee)) %>%
-    count(arrestee, wt = n) %>%
-    mutate(prcnt = n / sum(n)) %>%
-    pivot_longer(cols = arrestee, names_to = "actor", values_to = "status")
-    
-victims_df <-
-    victims %>%
+############################################################# Clean arrest data
+arrestees <-
+    open_dataset(here(arrestees_dir), schema = arrestee_schema) %>%
+    mutate(resident_status_of_arrestee = case_when(!(resident_status_of_arrestee %in% c("resident", "nonresident")) ~ "unknown",
+                                                   is.na(resident_status_of_arrestee) ~ "unknown",
+                                                   T ~ resident_status_of_arrestee),
+           race_of_arrestee = if_else(race_of_arrestee == "F",
+                                      "unknown",
+                                      race_of_arrestee),
+           ethnicity_of_arrestee = case_when(ethnicity_of_arrestee == "hispanic origin" ~ "hispanic",
+                                             ethnicity_of_arrestee == "not of hispanic origin" ~ "not hispanic",
+                                             is.na(ethnicity_of_arrestee) ~ "unknown",
+                                             T ~ "unknown"),
+           sex_of_arrestee = if_else(!(sex_of_arrestee %in% c("female", "male")),
+                                     "unknown",
+                                     sex_of_arrestee),
+           age_of_arrestee = case_when(str_sub(age_of_arrestee, 1, 1) == "0" ~ str_replace(age_of_arrestee, "0", ""),
+                                       age_of_arrestee == "over 98 years old" ~ "99",
+                                       age_of_arrestee == "unknown" ~ "-1",
+                                       T ~ age_of_arrestee),
+           age_of_arrestee = as.numeric(age_of_arrestee),
+           age_cat_a = case_when(age_of_arrestee >= 0 & age_of_arrestee <= 12 ~ "0 - 12",
+                                 age_of_arrestee >= 13 & age_of_arrestee <= 17 ~ "13 - 17",
+                                 age_of_arrestee >= 18 & age_of_arrestee <= 22 ~ "18 - 22",
+                                 age_of_arrestee >= 23 & age_of_arrestee <= 26 ~ "23 - 26",
+                                 age_of_arrestee >= 27 & age_of_arrestee <= 32 ~ "27 - 32",
+                                 age_of_arrestee >= 33 & age_of_arrestee <= 43 ~ "33 - 43",
+                                 age_of_arrestee >= 44 & age_of_arrestee <= 54 ~ "44 - 54",
+                                 age_of_arrestee >= 55 & age_of_arrestee <= 65 ~ "55 - 65",
+                                 age_of_arrestee >= 66 ~ "66 and older",
+                                 age_of_arrestee == -1 ~ "unknown"))
+       
+########################################################### Clean victim data
+# only keep individuals and officers, only ones with residential status.
+victims <-
+    open_dataset(here(victims_dir), schema = victim_schema) %>%
     filter(type_of_victim %in% c("individual", "law enforcement officer")) %>%
-    count(resident_status_of_victim) %>%
-    collect() %>%
-    rename(victim = resident_status_of_victim) %>%
-    mutate(victim = if_else(is.na(victim), "unknown", victim)) %>%
-    count(victim, wt = n) %>%
+    mutate(resident_status_of_victim = if_else(is.na(resident_status_of_victim),
+                                               "unknown",
+                                               resident_status_of_victim),
+           ethnicity_of_victim = case_when(ethnicity_of_victim == "hispanic origin" ~ "hispanic",
+                                           ethnicity_of_victim == "not of hispanic origin" ~ "not hispanic",
+                                           is.na(ethnicity_of_victim) ~ "unknown",
+                                           T ~ "unknown"),
+           age_of_victim = case_when(str_sub(age_of_victim, 1, 1) == "0" ~ str_replace(age_of_victim, "0", ""),
+                                     age_of_victim %in% c("1-6 days old", "7-364 days old", "under 24 hours (neonate)") ~ "0",
+                                     age_of_victim == "over 98 years old" ~ "99",
+                                     age_of_victim == "unknown" ~ "-1",
+                                     T ~ age_of_victim),
+           age_of_victim = as.numeric(age_of_victim),
+           age_cat_v = case_when(age_of_victim >= 0 & age_of_victim <= 12 ~ "0 - 12",
+                                 age_of_victim >= 13 & age_of_victim <= 17 ~ "13 - 17",
+                                 age_of_victim >= 18 & age_of_victim <= 22 ~ "18 - 22",
+                                 age_of_victim >= 23 & age_of_victim <= 26 ~ "23 - 26",
+                                 age_of_victim >= 27 & age_of_victim <= 32 ~ "27 - 32",
+                                 age_of_victim >= 33 & age_of_victim <= 43 ~ "33 - 43",
+                                 age_of_victim >= 44 & age_of_victim <= 54 ~ "44 - 54",
+                                 age_of_victim >= 55 & age_of_victim <= 65 ~ "55 - 65",
+                                 age_of_victim >= 66 ~ "66 and older",
+                                 age_of_victim == -1 ~ "unknown"))
+
+Query_Data_Arrestee <- function(.arrow_obj, ...) {
+    
+    .arrow_obj %>%
+        count(resident_status_of_arrestee, ...) %>%
+        collect() %>%
+        group_by(...) %>%
+        mutate(prcnt = n / sum(n)) %>%
+        ungroup() %>%
+        pivot_longer(cols = resident_status_of_arrestee,
+                     names_to = "actor",
+                     values_to = "status") %>%
+        mutate(actor = if_else(actor == "resident_status_of_arrestee",
+                               "arrestee",
+                               NA_character_))
+}
+
+Query_Data_Victim <- function(.arrow_obj, ...) {
+    
+    .arrow_obj %>%
+        count(resident_status_of_victim, ...) %>%
+        collect() %>%
+        group_by(...) %>%
+        mutate(prcnt = n / sum(n)) %>%
+        ungroup() %>%
+        pivot_longer(cols = resident_status_of_victim,
+                     names_to = "actor",
+                     values_to = "status") %>%
+        mutate(actor = if_else(actor == "resident_status_of_victim",
+                               "victim",
+                               NA_character_))
+}
+
+arrestees_df <- Query_Data_Arrestee(arrestees)
+arrestees_time_df <- Query_Data_Arrestee(arrestees, year)
+arrestees_race_df <- Query_Data_Arrestee(arrestees, race_of_arrestee, year)
+arrestees_ethnicity_df <- Query_Data_Arrestee(arrestees, ethnicity_of_arrestee, year)
+arrestees_sex_df <- Query_Data_Arrestee(arrestees, sex_of_arrestee, year)
+arrestees_age_df <- Query_Data_Arrestee(arrestees, age_cat_a, year)
+arrestees_state_time_df <- Query_Data_Arrestee(arrestees, state, year)
+arrestees_crime_df <-
+    arrestees %>%
+    filter(ucr_arrest_offense_code %in% c("drug/narcotic violations",
+                                          "simple assault",
+                                          "shoplifting",
+                                          "aggravated assault",
+                                          "all other larceny",
+                                          "destruction/damage/vandalism of property",
+                                          "burglary/breaking and entering",
+                                          "motor vehicle theft",
+                                          "robbery",
+                                          "disorderly conduct",
+                                          "theft from building",
+                                          "theft from motor vehicle",
+                                          "driving under the influence",
+                                          "rape",
+                                          "murder/nonnegligent manslaughter")) %>%
+    Query_Data_Arrestee(ucr_arrest_offense_code) %>%
+    filter(status == "resident") %>%
+    mutate(ucr_arrest_offense_code = fct_reorder(ucr_arrest_offense_code, prcnt))
+
+victims_df <- Query_Data_Victim(victims)
+victims_time_df <- Query_Data_Victim(victims, year)
+victims_race_df <- Query_Data_Victim(victims, race_of_victim, year)
+victims_ethnicity_df <- Query_Data_Victim(victims, ethnicity_of_victim, year)
+victims_sex_df <- Query_Data_Victim(victims, sex_of_victim, year)
+victims_age_df <- Query_Data_Victim(victims, age_cat_v, year)
+victims_state_df <- Query_Data_Victim(victims, state, year)
+victims_crime_df <-
+    victims %>%
+    filter(ucr_offense_code_1 %in% c("drug/narcotic violations",
+                                     "simple assault",
+                                     "shoplifting",
+                                     "aggravated assault",
+                                     "all other larceny",
+                                     "destruction/damage/vandalism of property",
+                                     "burglary/breaking and entering",
+                                     "motor vehicle theft",
+                                     "robbery",
+                                     "disorderly conduct",
+                                     "theft from building",
+                                     "theft from motor vehicle",
+                                     "driving under the influence",
+                                     "rape",
+                                     "murder/nonnegligent manslaughter")) %>%
+    Query_Data_Victim(ucr_offense_code_1) %>%
+    filter(status == "resident") %>%
+    mutate(ucr_offense_code_1 = fct_reorder(ucr_offense_code_1, prcnt))
+ggplot(victims_crime_df, aes(x = ucr_offense_code_1, y = prcnt)) + geom_point(aes(color = status)) +
+    theme(axis.text.x = element_text(hjust = 0, angle = 340))
+
+##################################### arrested vs. victim residential status
+################ inner join captures incidents where the offender was arrested
+Join_Victim_Arrestee <- function(victims_arrow, arrestees_arrow, year_i) {
+    
+    year_i <- as.character(year_i)
+    print(year_i)
+    
+    v <-
+        victims %>%
+        select(resident_status_of_victim, unique_incident_id, year) %>%
+        filter(year == year_i)
+    
+    a <-
+        arrestees %>%
+        select(resident_status_of_arrestee, unique_incident_id, year) %>%
+        filter(year == year_i)
+    
+    join <-
+        inner_join(v, a, by = c("unique_incident_id", "year")) %>%
+        count(resident_status_of_victim, resident_status_of_arrestee, year) %>%
+        collect()
+    
+    gc()
+    return(join)
+}
+
+join <-
+    map_dfr(1991:2021,
+            Join_Victim_Arrestee,
+            victims_arrow = victims,
+            arrestees_arrow = arrestees) %>%
+    group_by(year) %>%
     mutate(prcnt = n / sum(n)) %>%
-    pivot_longer(cols = victim, names_to = "actor", values_to = "status")
-
-resident_status <- bind_rows(arrestees_df, victims_df)
-
-ggplot(resident_status, aes(x = status, y = prcnt)) +
-    geom_bar(stat = "identity") +
-    facet_wrap(~actor) +
-    theme_bw()
+    ungroup()
